149fe6223c54d2b0d148ca2eb8fb506e50933014
Lucian Boboc <info@lucianboboc.com>
2023-12-27 22:28:38 +0100

    Created Router class

---
+++ Router.php
@@ -0,0 +1,89 @@
+<?php
+
+class Router
+{
+    public function registerRoute($method, $uri, $controller)
+    {
+        $this->routes[] = [
+            'method' => $method,
+            'uri' => $uri,
+            'controller' => $controller
+        ];
+    }
+
+    protected $routes = [];
+
+    /**
+     * Add a GET route
+     * @param $uri
+     * @param $controller
+     * @return void
+     */
+    public function get($uri, $controller)
+    {
+        $this->registerRoute('GET', $uri, $controller);
+    }
+
+    /**
+     * Add a POST route
+     * @param $uri
+     * @param $controller
+     * @return void
+     */
+    public function post($uri, $controller)
+    {
+        $this->registerRoute('POST', $uri, $controller);
+    }
+
+    /**
+     * Add a PUT route
+     * @param $uri
+     * @param $controller
+     * @return void
+     */
+    public function put($uri, $controller)
+    {
+        $this->registerRoute('PUT', $uri, $controller);
+    }
+
+    /**
+     * Add a DELETE route
+     * @param $uri
+     * @param $controller
+     * @return void
+     */
+    public function delete($uri, $controller)
+    {
+        $this->registerRoute('DELETE', $uri, $controller);
+    }
+
+    /**
+     * Route the request
+     * @param $uri
+     * @param $method
+     * @return void
+     */
+    public function route($uri, $method)
+    {
+        foreach ($this->routes as $route) {
+            if ($route['uri'] === $uri and $route['method'] === $method) {
+                require basePath($route['controller']);
+                return;
+            }
+        }
+
+        $this->error(404);
+    }
+
+    /**
+     * Load error page
+     * @param int $httpCode
+     */
+    private function error($httpCode = 404)
+    {
+        http_response_code($httpCode);
+        require basePath("controllers/error/{$httpCode}.php");
+        exit();
+
+    }
+}
--- public/index.php
+++ public/index.php
@@ -2,17 +2,13 @@
 
 require '../helpers.php';
 
-$routes = [
-    '/' => 'controllers/home.php',
-    '/listings' => 'controllers/listings/index.php',
-    '/listings/create' => 'controllers/listings/create.php',
-    '404' => 'controllers/error/404.php'
-];
+require basePath('Router.php');
+
+$router = new Router();
+
+require basePath('routes.php');
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$method = $_SERVER['REQUEST_METHOD'];
 
-if (array_key_exists($uri, $routes)) {
-    require basePath($routes[$uri]);
-} else {
-    require basePath($routes['404']);
-}
+$router->route($uri, $method);
