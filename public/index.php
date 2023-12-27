a9a4d70d143997a460a0bc015a2e80354d8f973e
Lucian Boboc <info@lucianboboc.com>
2023-12-27 21:38:32 +0100

    Added a basic router and views

--- public/index.php
+++ public/index.php
@@ -2,4 +2,17 @@
 
 require '../helpers.php';
 
-loadView('home');
+$routes = [
+    '/' => 'controllers/home.php',
+    '/listings' => 'controllers/listings/index.php',
+    '/listings/create' => 'controllers/listings/create.php',
+    '404' => 'controllers/error/404.php'
+];
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+if (array_key_exists($uri, $routes)) {
+    require basePath($routes[$uri]);
+} else {
+    require basePath($routes['404']);
+}
